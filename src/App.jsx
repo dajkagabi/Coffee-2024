e10bb18fbaf7a9ef82cf9e74c4216c5acf397bc5
Gabriella Dajka <dajkagabi01@gmail.com>
2024-11-10 19:32:15 +0100

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-
-import { BrowserRouter, Route,  Routes, Router  } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 
-
 function App() {
-
   return (
-    //Router 6.28.0
-    <BrowserRouter>
-      <Navbar/>
+    //6.28.0
+    <BrowserRouter future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true
+    }}>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/products' element={<Products/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products/>}/>
+        
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,13 @@
 
 
-const Products = () => {
+const Home = () => {
     return (
       <div>
         <h1>Welcome to the Products Page!</h1>
-        <p>This is the products page of the app.</p>
+        <p>This is the main page of the app.</p>
       </div>
     );
   };
   
-  export default Products;
+  export default Home;
   
