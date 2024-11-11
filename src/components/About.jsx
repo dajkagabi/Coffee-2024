3d530b49e546bb2800dfafe64397998e6e58f98d
Gabriella Dajka <dajkagabi01@gmail.com>
2024-11-11 21:07:26 +0100

    about stb

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,27 @@
 
+import './styles/About.css';
 
 const About = () => {
-    return (
-      <div>
-        <h1>Welcome to the About Page!</h1>
-        <p>This is the about page of the app.</p>
+  return (
+    <div className="container mt-5 text-color">
+      <div className="row">
+        <div className="col-md-8">
+          <h1 className="mb-4">Welcome!</h1>
+          <p>
+            I&apos;m a coffee enthusiast who loves exploring new flavors and roasting techniques.
+            I&apos;m excited to share my experiences and stories about those special coffees that have become part of my life.
+          </p>
+          <p>
+            Here, you&apos;ll find my latest coffee adventures, recipes, and tips and tricks for making the perfect cup of coffee.
+            Join me on this flavorful journey and discover the deeper layers of the coffee world!
+          </p>
+        </div>
+        <div className="col-md-4 text-center mb-4">
+          <img src="./public/images/coffeeImage.jpg" alt="Coffee Enthusiast" className="img-fluid align-right"/>
+        </div>
       </div>
-    );
-  };
-  
-  export default About;
-  
+    </div>
+  );
+}
+
+export default About;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import data from "./data/coffeeData.json";
 import './styles/Products.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function Products() {
   return (
@@ -9,10 +10,16 @@ function Products() {
         {data.map((coffee) => (
           <div key={coffee.id} className="col-md-4 mb-4">
             <div className="card">
-              <img src={coffee.image} className="card-img-top" alt={coffee.name} />
+              <img src={coffee.image} className="card-img-top img-fixed-height" alt={coffee.name} />
               <div className="card-body">
                 <h5 className="card-title">{coffee.name}</h5>
-                <p className="card-text">{coffee.description}</p>
+                <p className="card-text">{coffee.description}</p> 
+                <div className="d-flex justify-content-between align-items-center">
+                  <p className="card-price mb-0">${coffee.price}</p>
+                  <button className="btn btn-primary cart-button">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
